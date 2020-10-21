397db5f6f33b0825b99179d3a53530ebcf8dc662
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 21:37:21 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
-
+    intList = []
+    while(True):
+      number = int(input())
+      if(number==-1):
+        break
+      intList.append(number)
+    sum = 0
+    for i in range(len(intList)):
+      sum+=intList[i]
+    print("Sum: %s"%sum)
 if __name__ == '__main__':
     main()
